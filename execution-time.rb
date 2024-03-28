6c7d2554f5941ab60a98f66bd2aceed9e741bff2
katie leong <katieleong@yahoo.com>
2024-03-28 12:33:46 -0700

    worked on my_min

--- execution-time.rb
+++ execution-time.rb
@@ -1,13 +1,33 @@
-def my_min(arr)
+require 'byebug'
 
-    smallest_num = 0
+def my_min(arr) 
 
-    arr.each do |ele|
-        smallest_num = ele if smallest_num > ele
+    arr.each_with_index do |ele1, i|        #[ 0, 3, 5, 4, -5, 10, 1, 90 ]
+        arr.each_with_index do |ele2, j|      #[ 0, 3, 5, 4, -5, 10, 1, 90 ]
+            if ele1 < ele2 
+                return ele1
+                # debugger
+            end
+        end
+
     end
 
-    smallest_num
 end
+
+
+# def my_min(arr)
+
+#     smallest_num = 0
+
+#     arr.each do |ele|
+#         smallest_num = ele if smallest_num > ele
+#     end
+
+#     smallest_num
+# end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
+
+# time_complexity O(n)
+
