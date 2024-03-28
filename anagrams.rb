8f4d74a43c745019137c11f3829967535a9e312f
katie leong <katieleong@yahoo.com>
2024-03-28 16:10:18 -0700

    anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -3,21 +3,22 @@ def first_anagram?(str1, str2)
     two_d_arr = []
     array = str1.split("")
 
-    (str1.length * str1.length).times do
+    while two_d_arr.length < (str1.length * str1.length)
         sub_arr = []
 
-        until sub_arr.length == str1.length do 
+        until sub_arr.length == str1.length
             new_char = array.sample
             if !sub_arr.include?(new_char)
             sub_arr << new_char
             end
             
         end
-
+        if !two_d_arr.include?(sub_arr)
         two_d_arr << sub_arr.join("")
+        end
     end
+    two_d_arr.include?(str2)
 
-    two_d_arr
 end
 
 p first_anagram?("elvis", "lives")
