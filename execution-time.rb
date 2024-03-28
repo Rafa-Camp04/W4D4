e987645f383174ee393c571de85cfaf5f5befec9
Rafael Campos <rafaelcampos04@hotmail.com>
2024-03-28 17:03:53 -0400

    Work on phase 2 of largest_countinguous

--- execution-time.rb
+++ execution-time.rb
@@ -33,27 +33,45 @@ end
 # p my_min(list)  # =>  -5
 
 # time complexity: O(n^3)
+# def largest_contiguous_subsum(arr)
+#     array = []
+#     (0...arr.length).each do |start_idx|            # n^2
+#         (start_idx...arr.length).each do |end_idx|  
+#             array << arr[start_idx..end_idx]
+#         end
+#     end
+#     sum = []
+#     array.each do |sub_arr|                         # n
+#         sum << sub_arr.sum
+#     end
+#     return sum.max
+# end
+
 def largest_contiguous_subsum(arr)
-    array = []
-    (0...arr.length).each do |start_idx|            # n^2
-        (start_idx...arr.length).each do |end_idx|  
-            array << arr[start_idx..end_idx]
+
+    largest_sum = 0
+    current_sum = 0
+
+    (0...arr.length - 1).each_with_index do |i|
+        debugger
+        if (arr[i] + arr[i + 1]) > (arr[i] + arr[i + 1] + arr[i + 2])
+            current_sum += (arr[i] + arr[i + 1])
+
+        # current_sum += arr[i] + arr[i + 1] || current_sum += arr[i] + arr[i + 1] + arr[i + 2]
+
+        if current_sum > largest_sum
+            largest_sum = current_sum
         end
     end
-    sum = []
-    array.each do |sub_arr|                         # n
-        sum << sub_arr.sum
-    end
-    return sum.max
+
+    largest_sum
 end
 
-list = [5, 3, -7]
-p largest_contiguous_subsum(list) # => 8
+# list = [5, 3, -7]
+# p largest_contiguous_subsum(list) # => 8
 
-# possible sub-sums
-[5]           # => 5
-[5, 3]        # => 8 --> we want this one
-[5, 3, -7]    # => 1
-[3]           # => 3
-[3, -7]       # => -4
-[-7]          # => -7
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
