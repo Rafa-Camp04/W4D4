4d2f8d1383ad9b5af287a6918a2543356f9da922
Rafael Campos <rafaelcampos04@hotmail.com>
2024-03-28 16:01:31 -0400

    Work on largest contiguous sub-sum

--- execution-time.rb
+++ execution-time.rb
@@ -1,33 +1,34 @@
 require 'byebug'
 
+# time complexity O(n^2)
 def my_min(arr) 
 
-    arr.each_with_index do |ele1, i|        #[ 0, 3, 5, 4, -5, 10, 1, 90 ]
+    arr.each_with_index do |ele1, i| 
+        min = true                                         #[ 0, 3, 5, 4, -5, 10, 1, 90 ]
         arr.each_with_index do |ele2, j|      #[ 0, 3, 5, 4, -5, 10, 1, 90 ]
-            if ele1 < ele2 
-                return ele1
-                # debugger
+            if i == j
+                next
+            elsif ele1 > ele2
+                min = false 
             end
         end
-
+        return ele1 if min
     end
 
 end
 
+# time_complexity O(n)
+def my_min(arr)
 
-# def my_min(arr)
+    smallest_num = 0
 
-#     smallest_num = 0
+    arr.each do |ele|
+        smallest_num = ele if smallest_num > ele
+    end
 
-#     arr.each do |ele|
-#         smallest_num = ele if smallest_num > ele
-#     end
-
-#     smallest_num
-# end
+    smallest_num
+end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 p my_min(list)  # =>  -5
-
-# time_complexity O(n)
 
